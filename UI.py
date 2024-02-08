e01f661704ee84ede0c0280785621ea92430ad8b
Nolan Martinous <91758940+nmartinous@users.noreply.github.com>
2024-02-08 15:23:39 -0500

    Remove UI_Column_Based.py

--- UI.py
+++ UI.py
@@ -11,31 +11,6 @@ import time
 import streamlit as st 
 from langchain_community.llms import Ollama
 
-# Select the llm
-select = st.selectbox("Select Model: ", ["deepseek-coder", "tinyllama", "llama2"])
-
-# Ensure that the model variation is latest
-selected_model = select + ":latest"
-
-# -Add funtionality to choose between sizes, version, as well as download new models
-# -Ideally the model selection will be empty on first run and the user will be
-#   able to download any model that Ollama supports.
-# -This will likely be done by using a text file to keep track of the models the user
-#   downloaded so that the user can add and delete models. This file's contents would
-#   then be passed as the list for the selectbox above.
-# -In order to download new models, an 'Ollama run <model:version> call must be made
-#   to the terminal, followed by a '/bye' call to close it. This will likely be handled
-#   in a seperate script.
-# -Deletion involves a different call and will be handled later
-# -Model updating should be looked into as well
-# -Finding a way to automatically find which models ollama offers, as well as information
-#   about them should be researched.
-# -An alternate way of downloading and loading llms may also be useful, although Ollama
-#   seems to be the most practical at the time being. Having multiple options can not hurt.
-
-# Save the llm as an Ollama object
-llm = Ollama(model = selected_model)
-
 # Response generation
 def generate_response(prompt, history):
 
@@ -60,6 +35,31 @@ st.image('RSLogo.png')
 
 # Application title
 st.title("Generative AI Coding Chat")
+
+# Select the llm
+select = st.selectbox("Select Model: ", ["deepseek-coder", "tinyllama", "llama2"])
+
+# Ensure that the model variation is latest
+selected_model = select + ":latest"
+
+# -Add funtionality to choose between sizes, version, as well as download new models
+# -Ideally the model selection will be empty on first run and the user will be
+#   able to download any model that Ollama supports.
+# -This will likely be done by using a text file to keep track of the models the user
+#   downloaded so that the user can add and delete models. This file's contents would
+#   then be passed as the list for the selectbox above.
+# -In order to download new models, an 'Ollama run <model:version> call must be made
+#   to the terminal, followed by a '/bye' call to close it. This will likely be handled
+#   in a seperate script.
+# -Deletion involves a different call and will be handled later
+# -Model updating should be looked into as well
+# -Finding a way to automatically find which models ollama offers, as well as information
+#   about them should be researched.
+# -An alternate way of downloading and loading llms may also be useful, although Ollama
+#   seems to be the most practical at the time being. Having multiple options can not hurt.
+
+# Save the llm as an Ollama object
+llm = Ollama(model = selected_model)
 
 # Prompt the user 
 prompt = st.chat_input("Enter prompt:")
