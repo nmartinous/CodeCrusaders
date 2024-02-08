d19930ab3fd1fb0c6bfed6ba22b0f5ef380d3077
Nolan Martinous <91758940+nmartinous@users.noreply.github.com>
2024-02-08 10:30:47 -0500

    Styling and model change
    - Removed the column-based formatting and moved the original to a new file
    - Switched from tinyllama to deepseek-coder

--- UI.py
+++ UI.py
@@ -3,7 +3,7 @@ import streamlit as st
 from langchain_community.llms import Ollama
 
 # Save the llm as an Ollama object
-llm = Ollama(model="tinyllama:latest")
+llm = Ollama(model="deepseek-coder:latest")
 
 # Application title
 st.title("RiteSolutions GenAI Chat")
@@ -28,38 +28,33 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Create two columns, one with 99% width
-colA, colB = st.columns([.99, .01])
+# Prompt the user 
+prompt = st.chat_input("Enter prompt:")
+response = ""
+st.write("")
+st.write("")
 
-# In the first column:
-with colA:
 
-    # Prompt the user 
-    prompt = st.chat_input("Enter prompt:")
-    response = ""
-    st.write("")
-    st.write("")
+# After the user hits enter or clicks the send button
+if prompt:
 
-    # After the user hits enter or clicks the send button
-    if prompt:
+    # Start the response timer
+    start_time = time.time()
+    
+    # Add the prompt to the chat history as a user prompt
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-        # Start the response timer
-        start_time = time.time()
+    # Use generate_response method to create a response
+    response = generate_response(prompt, st.session_state.messages)
 
-        # Add the prompt to the chat history as a user prompt
-        st.session_state.messages.append({"role": "user", "content": prompt})
+    # Add the response to the chat history as an assistant response
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
-        # Use generate_response method to create a response
-        response = generate_response(prompt, st.session_state.messages)
+    # Output the response
+    st.write(response)
 
-        # Add the response to the chat history as an assistant response
-        st.session_state.messages.append({"role": "assistant", "content": response})
-
-        # Output the response
-        st.write(response)
-
-        # End the response timer, evaluate time to finsish, and output result
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        time_message = f"Model took {elapsed_time} seconds to respond"
-        st.success(time_message)
+    # End the response timer, evaluate time to finsish, and output result
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    time_message = f"Model took {elapsed_time} seconds to respond"
+    st.success(time_message)
---
+++ UI_Column_Based.py
@@ -0,0 +1,67 @@
+import time
+import streamlit as st 
+from langchain_community.llms import Ollama
+
+# Save the llm as an Ollama object
+llm = Ollama(model="tinyllama:latest")
+
+# Application title
+st.title("RiteSolutions GenAI Chat")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Response generation
+def generate_response(prompt, history):
+
+    # Create a prompt chain using past prompts and responses
+    prompt_chain = ""
+    for log in history:
+        prompt_chain += " " + log["content"]
+
+    # Return the response from the llm
+    return llm.invoke(prompt_chain)
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Create two columns, one with 99% width
+colA, colB = st.columns([.5, .5])
+
+# In the first column:
+with colA:
+
+    # Prompt the user 
+    prompt = st.chat_input("Enter prompt:")
+    response = ""
+    st.write("")
+    st.write("")
+
+# In the second column
+with colB:
+    # After the user hits enter or clicks the send button
+    if prompt:
+
+        # Start the response timer
+        start_time = time.time()
+
+        # Add the prompt to the chat history as a user prompt
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        # Use generate_response method to create a response
+        response = generate_response(prompt, st.session_state.messages)
+
+        # Add the response to the chat history as an assistant response
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+        # Output the response
+        st.write(response)
+
+        # End the response timer, evaluate time to finsish, and output result
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        time_message = f"Model took {elapsed_time} seconds to respond"
+        st.success(time_message)
