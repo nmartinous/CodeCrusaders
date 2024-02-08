c9ce9e272f80e90166407c3ddc6f31dc2f4c8918
Nolan Martinous <91758940+nmartinous@users.noreply.github.com>
2024-02-08 09:43:51 -0500

    Update UI.py
    - Added comments for all current code for future reference and enhanced collaboration

--- UI.py
+++ UI.py
@@ -1,8 +1,11 @@
 import time
 import streamlit as st 
 from langchain_community.llms import Ollama
+
+# Save the llm as an Ollama object
 llm = Ollama(model="tinyllama:latest")
 
+# Application title
 st.title("RiteSolutions GenAI Chat")
 
 # Initialize chat history
@@ -14,18 +17,37 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+# Create two columns, one with 99% width
 colA, colB = st.columns([.99, .01])
+
+# In the first column:
 with colA:
+
+    # Prompt the user 
     prompt = st.chat_input("Enter prompt:")
     response = ""
     st.write("")
     st.write("")
+
+    # After the user hits enter or clicks the send button
     if prompt:
+
+        # Start the response timer
         start_time = time.time()
+
+        # Add the prompt to the chat history as a user prompt
         st.session_state.messages.append({"role": "user", "content": prompt})
+
+        # Use the stored llm to respond to the prompt and save to response
         response = llm.invoke(prompt)
+
+        # Add the response to the chat history as an assistant response
         st.session_state.messages.append({"role": "assistant", "content": response})
+
+        # Output the response
         st.write(response)
+
+        # End the response timer, evaluate time to finsish, and output result
         end_time = time.time()
         elapsed_time = end_time - start_time
         time_message = f"Model took {elapsed_time} seconds to respond"
